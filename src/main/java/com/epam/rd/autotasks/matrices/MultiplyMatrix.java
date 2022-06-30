f8cf12c73a255f7c72720b30c9eadd00ad0f6fb6
Olha Mushket <olhamushket@gmail.com>
2022-07-01 00:33:55 +0300

    Solved

--- src/main/java/com/epam/rd/autotasks/matrices/MultiplyMatrix.java
+++ src/main/java/com/epam/rd/autotasks/matrices/MultiplyMatrix.java
@@ -3,10 +3,15 @@ import java.util.Arrays;
 
 public class MultiplyMatrix {
     public static int[][] multiply(int[][] matrix1, int[][] matrix2) {
-
-        // Put your code here
-
-        return null;
+        int [][] sum = new int[matrix1.length][matrix2[0].length];
+        for (int i=0; i<sum.length; i++) {
+            for (int j=0; j<sum[0].length; j++) {
+                for (int k=0; k<matrix2.length; k++) {
+                    sum[i][j]+=matrix1[i][k]*matrix2[k][j];
+                }
+            }
+        }
+        return sum;
     }
 
     public static void main(String[] args) {
@@ -16,14 +21,17 @@ public class MultiplyMatrix {
         // Get a result of your code
 
         int[][] a = {
-                {0, 12345},
-                {4509, 0},
-                {3, 567} };
-
+//                {0, 12345},
+//                {4509, 0},
+//                {3, 567} };
+                {1, 2, 3},
+                {4, 5, 6}};
         int[][] b = {
-                {653, 0, 25353},
-                {0, 61, 6} };
-
+//                {653, 0, 25353},
+//                {0, 61, 6} };
+                {7 , 8 },
+                {9 , 10},
+                {11, 12}};
         int[][] result = multiply(a, b);
         System.out.println(Arrays.deepToString(result).replace("],", "]\n"));
     }
